655bde426458f155fe8dc0f165cec3269b25061f
Raodapao <120090286@link.cuhk.edu.cn>
2024-07-10 15:43:27 +0800

    修复了一些 bug 并优化了性能

--- api.py
+++ api.py
@@ -18,7 +18,7 @@ def load_model_and_tokenizer(model_path):
     print("Loading model and tokenizer...")  # Debug print
     model = AutoModelForCausalLM.from_pretrained(
         model_path,
-        load_in_4bit=True,
+        load_in_8bit=True,
         optimize_model=True,
         trust_remote_code=True,
         use_cache=True, 
