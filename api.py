9aaff85459b0b43921d178fbb317dc2bbf9c493b
Raodapao <120090286@link.cuhk.edu.cn>
2024-07-12 17:21:43 +0800

    修复了一些 bug 并优化了性能

--- api.py
+++ api.py
@@ -1,120 +1,245 @@
-import torch
-import time
-from fastapi import FastAPI, HTTPException
+import asyncio
+from fastapi import APIRouter, UploadFile, File, Form, Body, Depends, Query
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 from pydantic import BaseModel
-from transformers import AutoTokenizer
-from ipex_llm.transformers import AutoModelForCausalLM
-import psutil
-import os
+from typing import Optional, Union, Dict, Any, List
+import json
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from threading import Thread
+import requests
+import re
+import ast
 import logging
 
-app = FastAPI()
-
-# Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# 配置日志
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Load model and tokenizer at startup
-model_path = "/home/qwen_intel/code/models/qwen2"
+text_splitter = CharacterTextSplitter(
+    separator="。",
+    chunk_size=5000,
+    chunk_overlap=0,
+    length_function=len,
+)
 
-def load_model_and_tokenizer(model_path):
-    logger.info("Loading model and tokenizer...")
-    model = AutoModelForCausalLM.from_pretrained(
-        model_path,
-        load_in_4bit=True,
-        optimize_model=True,
-        trust_remote_code=True,
-        use_cache=True,
-        max_memory={0: "15GB"},  # 为 XPU 设置最大内存使用量
-        torch_dtype=torch.float16
-    ).to("xpu")
-    
-    device_name = torch.xpu.get_device_name(0)  # 获取设备型号
-    logger.info(f"Model loaded on XPU: {device_name}")
-    
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_path,
-        trust_remote_code=True,
-    )
-    logger.info("Tokenizer loaded.")
-    return model, tokenizer
+storage = {}
 
-logger.info("Starting up and loading model/tokenizer...")
-model, tokenizer = load_model_and_tokenizer(model_path)
-logger.info("Model and tokenizer loaded at startup.")
+prompt_1 = """针对<正文>内容，撰写总结摘要。
+要求：
+ - 提取正文的主题
+ - 理解核心内容，并重新组织语言形成摘要
+ - 在摘要内容中，使用序号罗列要点
+ - 使用第三人称
+格式：
+    会议主题：<>
+    会议要点：
+        1. <>
+        2. <>
+        ...
+正文：
+ {text}
+摘要内容："""
 
-class RequestData(BaseModel):
-    system_setting: str
-    user_prompt: str
-    max_tokens: int = 1024  # Default max tokens to predict
+prompt_2 = """请根据<现有摘要>和<补充内容>撰写摘要。
+下面是现有摘要：{existing_answer}
+请根据<补充内容>完善现有摘要，形成一份新的摘要。
+请注意，新的摘要也要提供会议主题，并使用序号罗列要点。
+补充内容如下：
+------------
+{text}
+------------
+如果上面的补充内容对撰写摘要没有帮助，则直接返回现有摘要。"""
 
-@app.post("/generate-response_1.5b/")
-async def generate_response(data: RequestData):
-    logger.info("Received request.")
-    
-    messages = [
-        {"role": "system", "content": data.system_setting},
-        {"role": "user", "content": data.user_prompt}
-    ]
-    logger.info(f"Messages prepared: {messages}")
+class RetrieveRequest(BaseModel):
+    get_type: str = None
+    meeting_ids: Any = None
 
-    text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    logger.info(f"Text after applying chat template: {text}")
-    model_inputs = tokenizer([text], return_tensors="pt", truncation=True, max_length=2000).to("xpu")
-    logger.info(f"Model inputs prepared and moved to XPU: {model_inputs}")
-    
-    # Warmup generation
-    logger.info("Starting warmup generation...")
-    model.generate(model_inputs.input_ids, max_new_tokens=data.max_tokens)
-    logger.info("Warmup generation done.")
+def call_api(messages):
+    url = "http://localhost:8001/v1/chat/completions"
+    data = {
+        "model": "qwen2_7b_instruct",
+        "messages": messages,
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
 
-    # Actual generation with timing
+def async_summarizer(text_list, m_id, _revoke_url):
+    l = len(text_list) - 1
+    logger.info(f"m_id: {m_id}")
+    answer = ''
+    for i, line in enumerate(text_list):
+        if answer == '':
+            text_with_prompt = line
+        else:
+            text_with_prompt = line + "\n" + answer
+        response = call_api([
+            {"role": "user", "content": text_with_prompt},
+            {"role": "system", "content": "不要有重复内容，同时尽可能详细准确，序号标对"}
+        ])
+        answer =response.get('choices', [{}])[0].get('message', {}).get('content', '')
+        if i == l:
+            res = answer
+    result = {}
     try:
-        logger.info("Starting actual generation...")
-        
-        st = time.time()
-        generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=data.max_tokens)
-        torch.xpu.synchronize()
-        end = time.time()
-        logger.info("Generation done.")
+        topic = res.split('\n')[0]
+        content = '\n'.join(res.split('\n')[1:])
+    except:
+        topic = ''
+        content = res
+    tmp_topic = re.split(r'[:：]', topic)
+    if len(tmp_topic) > 1:
+        topic = '：'.join(re.split(r'[:：]', topic)[1:])
+    result['result'] = f"{res}"
+    result['topic'] = topic
+    result['content'] = content
+    result['msg'] = "succeed"
+    result['status'] = 200
+    result['meeting_id'] = m_id
+    logger.info(f"Result: {result}")
+    if _revoke_url:
+        requests.post(url=_revoke_url, json=jsonable_encoder(result))
+    storage[f"summ_v2_{m_id}"] = result
+    return result
 
-        generated_ids = generated_ids.cpu()
-        generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
-        response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        inference_time = end - st
+async def async_task(text_list, m_id, _revoke_url):
+    result = await asyncio.to_thread(async_summarizer, text_list, m_id, _revoke_url)
+    return result
 
-        # Monitor memory usage
-        mem_info = psutil.virtual_memory()
-        xpu_memory_info = torch.xpu.memory_stats()
-        
-        logger.info("Response generated.")
-        logger.info(f"Inference time: {inference_time}")
-        logger.info(f"Memory usage: {mem_info}")
-        logger.info(f"XPU memory usage: {xpu_memory_info}")
+router = APIRouter()
 
-        return {
-            "inference_time": inference_time,
-            "response": response,
-            "input_tokens": len(model_inputs.input_ids[0]),
-            "memory_usage": {
-                "total": mem_info.total,
-                "available": mem_info.available,
-                "percent": mem_info.percent,
-                "used": mem_info.used,
-                "free": mem_info.free
-            },
-            "xpu_memory_usage": xpu_memory_info,
-        }
+@router.post("/v2/summarizer")
+async def summarizer_server_v2(texts=Form(default=None), 
+                               args=Form(default=None), 
+                               file: Union[UploadFile, None] = None, 
+                               revoke_url=Form(default=None),
+                               meeting_id=Form(default=None),
+                               async_request=Form(default=None)):
+    logger.info(f"Received request with texts: {texts}, meeting_id: {meeting_id}, async_request: {async_request}")
+    m_id = meeting_id
+    _revoke_url = revoke_url
+    if file:
+        request = file.file.read()
+        request = request.decode()
+        logger.info(f"Plain text request: {request}")
+        request = request.replace('\n', '\\n')
+        request = request.replace('\/', r"/")
+        request = ast.literal_eval(request)
+        logger.info(f"Parsed request: {request}")
+        texts = request.get("texts")
+        m_id = request.get("meeting_id", m_id)
+        _revoke_url = request.get("revoke_url", _revoke_url)
+    elif texts:
+        texts = texts.replace('<br>', '\n')
+        logger.info(f"Texts: {texts}")
+        m_id = meeting_id
+    if not _revoke_url:
+        _revoke_url = revoke_url
 
-    except Exception as e:
-        logger.error(f"Error during actual generation: {str(e)}")
-        raise HTTPException(status_code=500, detail="Generation failed: " + str(e))
+    logger.info(f"Final texts: {texts}, meeting_id: {m_id}, revoke_url: {_revoke_url}")
 
-# Run the app
+    text_list = text_splitter.split_text(texts)
+    l = len(text_list) - 1
+    answer = ''
+    res = ''
+    if async_request:
+        t = Thread(target=async_summarizer, args=(text_list, m_id, _revoke_url))
+        t.start()
+        return {"meeting_id": m_id}
+    else:
+        for i, line in enumerate(text_list):
+            if answer == '':
+                text_with_prompt = prompt_1.replace("{text}", line)
+            else:
+                text_with_prompt = prompt_2.replace("{text}", line).replace("{existing_answer}", answer)
+
+            answer =call_api([
+                {"role": "user", "content": text_with_prompt},
+                {"role": "system", "content": "不要有重复内容，同时尽可能详细准确，序号标对"}
+            ]).get('choices', [{}])[0].get('message', {}).get('content', '')
+            if i == l:
+                res = answer
+        result = {}
+        try:
+            topic = res.split('\n')[0]
+            content = '\n'.join(res.split('\n')[1:])
+        except:
+            topic = ''
+            content = res
+        tmp_topic = re.split(r'[:：]', topic)
+        if len(tmp_topic) >= 1:
+            topic = '：'.join(re.split(r'[:：]', topic)[1:])
+        result['result'] = f"{res}"
+        result['topic'] = topic
+        result['content'] = content
+        result['msg'] = "succeed"
+        result['status'] = 200
+        result['meeting_id'] = m_id
+        storage[f"summ_v2_{m_id}"] = result
+        logger.info(f"Result: {result}")
+        if _revoke_url:
+            requests.post(url=_revoke_url, json=jsonable_encoder(result))
+        return result
+
+@router.post("/v2/retrieveSummarizer")
+async def retrieve_summarizer_v2(request: RetrieveRequest):
+    logger.info(f"Received retrieve request: {request}")
+    logger.info(f"Storage keys: {storage.keys()}")
+    meeting_ids = json.loads(request.meeting_ids)
+    response = []
+    for mid in meeting_ids:
+        if isinstance(mid, str):
+            if f"summ_v2_{mid}" in storage:
+                resp = storage[f"summ_v2_{mid}"]
+                if not resp.get('msg'):
+                    resp['result'] = None
+                    response.append(resp)
+                else:
+                    response.append(resp)
+                    del storage[f"summ_v2_{mid}"]
+            else:
+                response.append({"meeting_id": mid, "result": None, "stop": True})
+        else:
+            mid_id = mid['meeting_id']
+            if f"summ_v2_{mid_id}" in storage:
+                resp = storage[f"summ_v2_{mid_id}"]
+                if not resp.get('msg'):
+                    resp['result'] = None
+                    response.append(resp)
+                else:
+                    response.append(resp)
+                    del storage[f"summ_v2_{mid_id}"]
+            else:
+                response.append({"meeting_id": mid_id, "result": None, "stop": True})
+    logger.info(f"Retrieve response: {response}")
+    return jsonable_encoder(response)
+
+port = 8000
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+def start_server():
+    uvicorn.run(
+        "modify:app",
+        host="0.0.0.0",
+        port=port,
+        workers=1
+    )
+
 if __name__ == "__main__":
-    logger.info("Starting the app...")
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    logger.info("App is running.")
+    start_server()
